1cd34b3b2c5ef9d8e734bbf52bcf2bc162a1b2bd
Daniel Marques <daniel@descentralizado.com.br>
2022-10-24 16:13:51 -0300

    feat: network params

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+export const networks = [
+    {
+        network: 'goerli',
+        contract: '0x2B740a656C86E00Fd7b5Ba9FbC3F657239C58d71',
+        chainId: '0x5',
+        explorer: 'https://goerli.etherscan.io/'
+    },
+    {
+        network: 'ethereum',
+        contract: '0xD152f549545093347A162Dce210e7293f1452150',
+        chainId: '0x1',
+        explorer: 'https://etherscan.io/'
+    }
+]
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 import React, {useEffect, useState} from 'react';
+import {ethers} from 'ethers';
+import type {BigNumber} from 'ethers';
+
 import {getProvider, getAbi, disperseInterface} from '../../utils/disperse'
+import {networks} from '../../config'
 
 import DisperseIcon from '../../assets/disperse.svg'
 import EtherscanIcon from '../../assets/etherscan.svg'
@@ -7,26 +11,154 @@ import GithubIcon from '../../assets/github.svg'
 import TelegramIcon from '../../assets/telegram.svg'
 import DisperseBody from '../../assets/disperseBody.svg'
 
-import {Container, Header, Disperse, SocialMediaLink, Link, Footer, Contents, Body, MetamaskWallet, Send, Balance, Recipients, IconBody} from  './style'
+import {Container, 
+    Header, 
+    Disperse, 
+    SocialMediaLink, 
+    Link, 
+    Footer, 
+    Contents, 
+    Body, 
+    MetamaskWallet, 
+    Send, 
+    Balance, 
+    Recipients, 
+    IconBody
+} from  './style'
+
+interface Network {
+    network: string,
+    contract: string,
+    chainId: string,
+    explorer: string
+}
+
+interface ContractParams {
+    addresses: Array<string>,
+    values: Array<BigNumber>,
+    totalValues: number
+}
+
+// 0x8ab7A8A6e6eFbC725A87CB0F185d0B11C4bd2b0D 0.01
+// 0x1366ca6056668A0F45040C7d01189d575b8374fE,0.02
+// 0x459E15D31956a01808FD1F16375dffBA005ae36A=1.0
+
 
 const Home: React.FC = () => {
-    const [address, setAddress] = useState('')
-    const [chainId, setChainId] = useState('')
-    const [balance, setBalance] = useState('')
+    const [address, setAddress] = useState<string>('')
+    const [chainId, setChainId] = useState<string>('')
+    const [recepientsAmounts, setRecepientsAmounts] = useState<string>('')
+    const [balance, setBalance] = useState<string>('')
+    const [network, setNetwork] = useState<Network>({
+        network: '',
+        contract: '',
+        chainId: '',
+        explorer: ''
+    })
 
+    const parseRecepientsAmounts = async (): Promise<ContractParams> =>  {
+        const params = recepientsAmounts.split('\n')
+        if(params.length > 2){
+            const addresses = [];
+            const values = [];
+            let totalValues = 0;
+
+            const wrongAddress = [];
+
+            for (let i = 0; i < params.length; i++) {
+                const param = params[i];
+                const splitParam = param.split(/[=,\s]/)
+                const [addressParam, valueParam] = splitParam;
+                if(addressParam.includes('0x') && ethers.utils.isAddress(addressParam)){
+                    addresses.push(addressParam)
+                    values.push(ethers.utils.parseEther(valueParam))
+                    totalValues += parseFloat(valueParam);
+                }else{
+                    addressParam !== '' ? wrongAddress.push({address: addressParam, values: valueParam }) : ''
+                }
+            }
+
+            if(wrongAddress.length > 0) alert(`addresses ou values wrong: ${JSON.stringify(wrongAddress)} `)
+
+            if(totalValues > parseFloat(balance)) {
+                if(addresses.length === values.length){
+                    return {addresses, values, totalValues};
+                }else{
+                    alert(`addresses and values no is equal`)
+                }
+            }else{
+                alert(`total is greater than balance account`)
+            }
+
+            return {addresses: [], values: [], totalValues: 0}
+        }
+    }
+
+    const handleOnChange = (e: any) => {
+        const value = e.target.value
+        setRecepientsAmounts(value);
+    }
+
+    const getinterfaceContract = () => {
+        const provider = getProvider((window as any).ethereum);
+        const abi = getAbi();
+        const disperse = disperseInterface(network.contract, abi, provider.getSigner())
+
+        return disperse;
+    }
+
+    const disperseEth = async () => {
+        try {
+            const {addresses, values, totalValues} = await parseRecepientsAmounts();
+            const disperse = getinterfaceContract();
+            await disperse.disperseEther([addresses, values], {values: totalValues})
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+       
+
+    const disperseToken = async () => {
+        const disperse = getinterfaceContract();
+        await disperse.disperseToken()
+    }
+
+    const connectWallet = async () => {
+        if (typeof window.ethereum !== 'undefined') {
+            const accounts = await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
+            const id = await (window as any).ethereum.request({ method: 'eth_chainId' })
+
+            setChainId(id)
+            setAddress(accounts[0])
+        }else{
+            alert('MetaMask no is installed!');
+        }
+    }
 
     useEffect(() => {
+        (window as any).ethereum.on('chainChanged', (id: string) => {
+            setChainId(id)
+        });
+
+        (window as any).ethereum.on('accountsChanged', (accounts: Array<string>) => {
+            setAddress(accounts[0])
+        });
+
         connectWallet();
     })
 
     useEffect(() => {
-        if(chainId !== '0x6b5' && chainId !== '0x521' && chainId !== '' ){
+        const networkIndex = networks.findIndex(networkConfig => networkConfig.chainId === chainId)
+
+        if(networkIndex === -1 && chainId !== ''){
             alert('network invalid')
+        }else{
+            setNetwork(networks[networkIndex])
         }
     },[chainId])
 
     useEffect(() => {
-        console.log(address)
         if(address !== '') getBalance(address)
     },[address])
 
@@ -37,42 +169,6 @@ const Home: React.FC = () => {
         setBalance(decimalBalance.toFixed(5).toString());
     }
 
-    const getinterfaceContract = async () => {
-        const provider = await getProvider((window as any).ethereum);
-        const abi = await getAbi();
-        const disperse = await disperseInterface('0x40f467bC1f56cCADEf91E2EB3BDd77c8Bfe7936F', abi, provider.getSigner())
-
-        return disperse;
-    }
-
-    const disperseEth = async  () => {
-        const disperse = await getinterfaceContract();
-        await disperse.disperseEther()
-    }
-
-    const disperseToken = async () => {
-
-    }
-
-    const connectWallet = async () => {
-        if (typeof window.ethereum !== 'undefined') {
-            const accounts = await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
-            
-            (window as any).ethereum.on('chainChanged', (id: string) => {
-                console.log(id)
-                setChainId(id)
-            });
-
-            (window as any).ethereum.on('accountsChanged', (accounts: Array<string>) => {
-                setAddress(accounts[0])
-            });
-
-            setAddress(accounts[0])
-        }else{
-            console.log('MetaMask no is installed!');
-        }
-    }
-
     return (
         <Container>
             <Header>
@@ -80,7 +176,7 @@ const Home: React.FC = () => {
                 <SocialMediaLink>
                     <Link>
                         <img src={EtherscanIcon} alt="etherscan"></img>
-                        <a href={'https://etherscan.io/address/0xd152f549545093347a162dce210e7293f1452150'}>Etherscan</a>
+                        <a href={`${network?.explorer}address/${network?.contract}`}>Etherscan</a>
                     </Link>
                     <Link>
                         <img src={GithubIcon} alt="github"></img>
@@ -101,20 +197,19 @@ const Home: React.FC = () => {
                             {address !== '' ? (
                                 <>
                                     <p>logged in as </p>
-                                    <a href={`https://etherscan.io/address/${address}`}>{address}</a>
+                                    <a href={`${network?.explorer}address/${address}`}>{address}</a>
                                 </>
                             ):(
                                 <>
                                     <button onClick={() => connectWallet()}>Connect</button>
                                 </>
                             )}
-                            
                         </div>
                     </MetamaskWallet>
                     <Send>
                         <h3>Send ether or token</h3>
                         <div>
-                            <button>ETHER</button>
+                            <button onClick={() => {network.chainId !== '' ? disperseEth() : ''}}>ETHER</button>
                             <button>TOKENS</button>
                         </div>
                     </Send>
@@ -133,7 +228,7 @@ const Home: React.FC = () => {
             <Recipients>
                 <h3>Recipients and Amounts</h3>
                 <div>
-                    <textarea  placeholder="support any format&#10;0x314ab97b76e39d63c78d5c86c2daf8eaa306b182 3.141592&#10;0x271bffabd0f79b8bd4d7a1c245b7ec5b576ea98a,2.7182&#10;0x141ca95b6177615fb1417cf70e930e102bf8f584=1.41421"></textarea>
+                    <textarea value={recepientsAmounts} onChange={(e) => handleOnChange(e)} placeholder="support any format&#10;0x314ab97b76e39d63c78d5c86c2daf8eaa306b182 3.141592&#10;0x271bffabd0f79b8bd4d7a1c245b7ec5b576ea98a,2.7182&#10;0x141ca95b6177615fb1417cf70e930e102bf8f584=1.41421"></textarea>
                 </div>
             </Recipients>
             <Footer>
